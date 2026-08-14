e876979a8ebe7f9dfc3067881b8d5f66504b55a2
Villads Alexander <villads@e-id.dk>
2026-08-14 13:46:26 +0200

    Lav nye farver til terminalen, og udskriv en linje for at teste

---
+++ VilladsPong/Program.cs
@@ -0,0 +1,6 @@
+﻿//lav nye farver ud fra rgb
+String hackerGreen = "\x1b[38;2;0;255;0m";
+String hackerRed = "\x1b[38;2;255;0;0m";
+String reset = "\x1b[0m";
+//udskriv en linje
+Console.WriteLine($"{hackerGreen}Hello, World! My name is Mr Robbort \nWelcome to my {hackerRed}EVIL!!! {hackerGreen}pong!!!{reset}");
